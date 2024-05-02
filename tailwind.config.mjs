1f7708d814b3bbe2a4e03402d64728147152f4d3
twinik <tobiwinik@gmail.com>
2024-05-02 20:45:01 -0300

    little fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import animations from "@midudev/tailwind-animations";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -18,5 +20,5 @@ export default {
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("flowbite/plugin"), animations],
 };
